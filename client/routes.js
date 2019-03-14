c63f0ec5a38ec4f89951695bd50559933983059b
Stephanie Coy <scoy028@gmail.com>
2019-03-14 15:56:24 -0400

    button navigates from overview to step

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome, RecipeStep} from './components'
+import {Login, Signup, UserHome, RecipeStep, RecipeOverview} from './components'
 import {me} from './store'
 
 /**
@@ -22,6 +22,7 @@ class Routes extends Component {
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         <Route path="/recipe-step" component={RecipeStep} />
+        <Route path="/recipe" component={RecipeOverview} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
