0c59e808ed57a4b10e8792ca1c066ee4c20f1676
Stephanie <36521666+scoy028@users.noreply.github.com>
2019-03-16 11:49:47 -0400

    Voice features (#23)
    * working web speech
    
    * add anyang
    
    * added annyang cases to recipe-step

--- client/components/recipe-step.js
+++ client/components/recipe-step.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react'
+import annyang from 'annyang'
+
 // import { speak, repeatStep, listIngredients, listenForCommand } from '../helper-funcs'
 
 const speak = words => {
@@ -15,60 +17,115 @@ const listIngredients = () => {
 }
 
 export default class RecipeStep extends Component {
-  constructor() {
-    super()
+  // listenForCommand = event => {
+  //   const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+  //   const recognition = new SpeechRecognition()
+
+  //   recognition.continuous = true
+  //   recognition.interimResults = false
+  //   recognition.start()
+  //   // eslint-disable-next-line complexity
+  //   recognition.onresult = function(event) {
+  //     if (event.results.length > 0) {
+  //       const latest = Object.keys(event.results).length - 1
+  //       let result = event.results[latest][0].transcript.split(' ')
+  //       result = result[result.length - 1]
+
+  //       console.log('event.results', event.results)
+  //       console.log('result', result)
+  //       console.log('Speaking? ', speechSynthesis.speaking)
+
+  //       switch (result) {
+  //         case 'next': {
+  //           console.log('Next command')
+  //           break
+  //         }
+  //         case 'back' || 'backed' || 'backs' || 'go back': {
+  //           console.log('Back command')
+  //           break
+  //         }
+  //         case 'repeat' || 'repeats': {
+  //           console.log('Repeat command: ', result)
+  //           repeatStep()
+  //           break
+  //         }
+  //         case 'ingredients': {
+  //           console.log('Ingredients command', result)
+  //           listIngredients()
+  //           break
+  //         }
+  //         default: {
+  // const repeatRequest = "Sorry, I didn't get that. Please try again."
+  // recognition.stop()
+  // speak(repeatRequest)
+  // window.setTimeout(() => {
+  //   recognition.start()
+  // }, 4000)
+  // break
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+
+  annyang = () => {
+    if (annyang) {
+      var commands = {
+        // 'repeat': repeatStep,
+        // 'repeats': repeatStep,
+        // 'ingredient': listIngredients,
+        // 'ingredients': listIngredients
+        'hey julia': this.nullCommand,
+        'hey julia help': this.help,
+        'hey julia *command': this.command
+      }
+      annyang.addCommands(commands)
+      annyang.start()
+    }
   }
 
-  listenForCommand = event => {
-    const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-    const recognition = new SpeechRecognition()
-    // if (event.target.value === 'Click to Pause') {
-    //   console.log('inside listen pause')
-    //   // recognitionadd.abort()
-    // }
-    recognition.continuous = true
-    recognition.interimResults = false
-    console.log('inside ListenForCommand')
-    recognition.start()
-    recognition.onresult = function(event) {
-      if (event.results.length > 0) {
-        const latest = Object.keys(event.results).length - 1
-        let result = event.results[latest][0].transcript.split(' ')
-        result = result[result.length - 1]
-        console.log('event.results', event.results)
-        console.log('result', result)
-        switch (result) {
-          case 'next': {
-            console.log('Next command')
-            break
-          }
-          case 'back' || 'backed' || 'backs' || 'go back': {
-            console.log('Back command')
-            break
-          }
-          case 'repeat' || 'repeats': {
-            console.log('Repeat command: ', result)
-            // this.repeatStep()
-            repeatStep()
-            break
-          }
-          case 'ingredients': {
-            console.log('Ingredients command', result)
-            listIngredients()
-            break
-          }
-          default: {
-            const repeatRequest = "Sorry, I didn't get that. Please try again."
-            recognition.stop()
-            speak(repeatRequest)
-            window.setTimeout(() => {
-              console.log('inside setTimeout')
-              recognition.start()
-              // document.getElementById('command').click
-            }, 4000)
-            break
-          }
-        }
+  help = () => {
+    const repeatRequest =
+      'You can ask me any of the following: Repeat, Ingredients, Back, Next, or Pause.'
+    speak(repeatRequest)
+  }
+
+  nullCommand = () => {
+    const repeatRequest = 'How can I help you?'
+    speak(repeatRequest)
+  }
+
+  command = command => {
+    console.log('in command', `_${command}_`)
+    switch (command) {
+      case null: {
+        console.log('in null command')
+      }
+      case 'repeat': {
+        console.log('in repeat')
+        repeatStep()
+        break
+      }
+      case 'repeats': {
+        repeatStep()
+        break
+      }
+      case 'ingredient': {
+        listIngredients()
+        break
+      }
+      case 'ingredients': {
+        listIngredients()
+        break
+      }
+      default: {
+        const repeatRequest = "Sorry, I didn't get that. Please try again."
+        annyang.pause()
+        speak(repeatRequest)
+        window.setTimeout(() => {
+          annyang.resume()
+        }, 4000)
+        break
       }
     }
   }
@@ -88,29 +145,21 @@ export default class RecipeStep extends Component {
           </ol>
         </div>
         <div id="step-instructions">
-          <p>step 1</p>
+          <p>
+            Combine flour, white sugar, baking powder and salt. In a separate
+            bowl, mix together egg, milk, vegetable oil and bananas.
+          </p>
         </div>
         <div id="timer">
           <p>timer</p>
         </div>
-        {/* conditional if bake or not at all */}
-        <div id="quantities">
-          <ul>
-            <li>quantity 1</li>
-            <li>quantity 2</li>
-          </ul>
-        </div>
         <div>
           <button type="button">Back</button>
-          <button
-            id="command"
-            type="button"
-            value="Click to Give Command"
-            onClick={this.listenForCommand}
-          >
-            Give Command
+          <button id="command" type="button" onClick={this.annyang}>
+            Start
           </button>
-          <button type="button">Start/Pause</button>
+          {/* change to resume once annyang is in componentDidMount */}
+          <button type="button">Pause</button>
           <button type="button">Next</button>
         </div>
       </div>
