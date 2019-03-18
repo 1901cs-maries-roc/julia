90c7603e79bc9551b4bc390babbb07a2bcb2aa2d
celinechole <github@celinechole.com>
2019-03-18 13:11:43 -0400

    fix: remove numbers in front of steps

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -3,26 +3,13 @@ const Recipe = require('./recipe')
 const Tag = require('./tag')
 const Ingredient = require('./ingredient')
 const db = require('../db')
-const Sequelize = require('sequelize')
-
-// const recipeIngredient = db.define('recipeIngredient', {
-//   quantity: {
-//     type: Sequelize.STRING,
-//     allowNull: false
-//   },
-//   sectionName: {
-//     type: Sequelize.STRING,
-//     allowNull: true,
-//     defaultValue: null
-//   }
-// })
 
 const tagRecipe = db.define('tagRecipe')
 
 Recipe.belongsTo(User)
 User.hasMany(Recipe)
-// Ingredient.belongsTo(Recipe)
-// Recipe.hasMany(Ingredient)
+Ingredient.belongsTo(Recipe)
+Recipe.hasMany(Ingredient)
 // Ingredient.belongsToMany(Recipe, {through: recipeIngredient})
 // Recipe.belongsToMany(Ingredient, {through: recipeIngredient})
 Tag.belongsToMany(Recipe, {through: tagRecipe})
