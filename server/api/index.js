6c528b20b7ac8e1a100b5367c3cfd1ce3addc652
Wendy Sung <wendalssung@gmail.com>
2019-03-20 16:52:40 -0400

    feat(scraping): add img url

--- server/api/index.js
+++ server/api/index.js
@@ -11,14 +11,11 @@ router.post('/scrape', (req, res, next) => {
   request(req.body.url, (error, response, html) => {
     if (!error) {
       const $ = cheerio.load(html)
-      const instructions = []
-      const ingredients = []
       const name = $('title')
         .eq(0)
         .text()
-      const instructionLists = $('ol').filter((i, elem) => {
-        return $(elem).attr('class') !== 'comment-list'
-      })
+      const ingredients = []
+      const instructions = []
       const ingredientsLabel = $(':contains("Ingredients")').filter(
         (i, elem) => {
           return (
@@ -27,6 +24,9 @@ router.post('/scrape', (req, res, next) => {
           )
         }
       )
+      const instructionLists = $('ol').filter((i, elem) => {
+        return $(elem).attr('class') !== 'comment-list'
+      })
 
       instructionLists.find('li').each((i, elem) => {
         instructions[i] = $(elem).text()
@@ -38,7 +38,24 @@ router.post('/scrape', (req, res, next) => {
         .each((i, elem) => {
           ingredients[i] = $(elem).text()
         })
+
+      let imgUrl
+      let maxDimensions = 0
+      $('img').each((i, elem) => {
+        const imgDimensions =
+          Number($(elem).attr('height')) * Number($(elem).attr('width'))
+        // console.log("maxD: ", maxDimensions)
+        // console.log("currentD: ", imgDimensions)
+        if (imgDimensions > maxDimensions) {
+          imgUrl = $(elem).attr('data-src') || $(elem).attr('src')
+          // console.log('img taller than last: ', $(elem).attr('data-src'))
+          maxDimensions =
+            Number($(elem).attr('height')) * Number($(elem).attr('width'))
+        }
+      })
+
       console.log('Title: ', name)
+      console.log('imgUrl: ', imgUrl)
       console.log('Ingredients: ', ingredients)
       console.log('Instructions: ', instructions)
 
