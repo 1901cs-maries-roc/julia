09f66fab4d08e1cee56befe146e45306d93b953f
Stephanie Coy <scoy028@gmail.com>
2019-03-21 17:32:34 -0400

    feat(form): fleshed out recipes form with all fields

--- client/components/recipe-form.js
+++ client/components/recipe-form.js
@@ -6,21 +6,83 @@ class RecipeForm extends Component {
   render() {
     return (
       <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
+        <Form.Row>
+          <Form.Group controlId="formPrepTime">
+            <Form.Label>Prep Time</Form.Label>
+            <Form.Control type="prepTime" />
+          </Form.Group>
+          {/* </Form.Row>
 
-        <Form.Group controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group controlId="formBasicChecbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
+        <Form.Row> */}
+          <Form.Group controlId="formCookTime">
+            <Form.Label>Cook Time</Form.Label>
+            <Form.Control type="cookTime" />
+          </Form.Group>
+          {/* </Form.Row>
+
+        <Form.Row> */}
+          <Form.Group controlId="formWaitTime">
+            <Form.Label>Wait Time</Form.Label>
+            <Form.Control type="waitTime" />
+          </Form.Group>
+          {/* </Form.Row>
+
+        <Form.Row> */}
+          <Form.Group controlId="servings">
+            <Form.Label>Number of servings</Form.Label>
+            <Form.Control type="servings" />
+          </Form.Group>
+        </Form.Row>
+
+        <Form.Row>
+          <Form.Group controlId="formTitle">
+            <Form.Label>Recipe Title</Form.Label>
+            <Form.Control type="title" placeholder="Enter title" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+        </Form.Row>
+
+        <Form.Row>
+          <Form.Group controlId="formIngredients">
+            <Form.Label>Ingredients</Form.Label>
+            <Form.Control
+              type="ingredients"
+              placeholder="Enter one ingredient"
+            />
+            <Form.Text className="text-muted">
+              Enter each ingredient on its own line.
+            </Form.Text>
+          </Form.Group>
+        </Form.Row>
+
+        <Form.Row>
+          <Form.Group controlId="formInstructions">
+            <Form.Label>Instructions</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows="2"
+              placeholder="Enter one instruction"
+            />
+            <Form.Text className="text-muted">
+              Enter each instruction on its own line.
+            </Form.Text>
+          </Form.Group>
+        </Form.Row>
+
+        <Form.Row>
+          <Form.Group controlId="formTags">
+            <Form.Label>Tags</Form.Label>
+            <Form.Control as="select">
+              <option>Choose...</option>
+              <option>Easy</option>
+              <option>Intermediate</option>
+              <option>Hard</option>
+            </Form.Control>
+          </Form.Group>
+        </Form.Row>
+
         <Button variant="primary" type="submit">
           Submit
         </Button>
