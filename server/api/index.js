94ec7a692ecfb743184712fcb9251badfb228dd3
Wendy Sung <wendalssung@gmail.com>
2019-03-21 18:19:26 -0400

    add prep, cook, and total times

--- server/api/index.js
+++ server/api/index.js
@@ -47,26 +47,79 @@ const findImg = $ => {
   return imgUrl
 }
 
-const findPrepTime = ($, prepTimeRoot) => {
-  return prepTimeRoot.text().trim()
+const extractTime = rootEl => {
+  const num = /\d/
+  const unit = /\sm|\sh/
+  const timeIndex = rootEl.search(num)
+  const unitIndex = rootEl.search(unit) + 1
+  // console.log(">>rootEL: ", rootEl)
+  // console.log(">>time/unit: ", rootEl[timeIndex], "/", rootEl[unitIndex])
+  const time = rootEl.slice(timeIndex, unitIndex)
+  return rootEl[unitIndex] === 'h' ? time * 60 : time
 }
 
-const findCookTime = ($, prepTimeRoot) => {
-  return prepTimeRoot.nextAll().text()
+const findPrepTime = $ => {
+  const rootEl = $(':contains("Prep")')
+    .filter((i, elem) => {
+      const r = /^Prep/
+      return r.test(
+        $(elem)
+          .text()
+          .trim()
+      )
+    })
+    .text()
+    .trim()
+  return extractTime(rootEl)
+}
+
+const findCookTime = $ => {
+  const rootEl = $(':contains("Cook")')
+    .filter((i, elem) => {
+      const r = /^Cook\s|^Cook\d/
+      return r.test(
+        $(elem)
+          .text()
+          .trim()
+      )
+    })
+    .text()
+    .trim()
+  return extractTime(rootEl)
+}
+
+const findTotalTime = $ => {
+  const rootElA = $(':contains("Total Time")')
+    .filter((i, elem) => {
+      const r = /^Total Time/
+      return r.test(
+        $(elem)
+          .text()
+          .trim()
+      )
+    })
+    .text()
+    .trim()
+
+  const rootElB = $(':contains("Ready In")')
+    .filter((i, elem) => {
+      const r = /^Ready In/
+      return r.test(
+        $(elem)
+          .text()
+          .trim()
+      )
+    })
+    .text()
+    .trim()
+  const rootEl = rootElA || rootElB
+  return extractTime(rootEl)
 }
 
 router.post('/scrape', (req, res, next) => {
   request(req.body.url, (error, response, html) => {
     if (!error) {
       const $ = cheerio.load(html)
-      const prepTimeRoot = $(':contains("Prep")').filter((i, elem) => {
-        const r = /^Prep/
-        return r.test(
-          $(elem)
-            .text()
-            .trim()
-        )
-      })
 
       const name = $('title')
         .first()
@@ -74,13 +127,15 @@ router.post('/scrape', (req, res, next) => {
       const instructions = findInstructions($)
       const ingredients = findIngredients($)
       const imgUrl = findImg($)
-      const prepTime = findPrepTime($, prepTimeRoot)
-      const cookTime = findCookTime($, prepTimeRoot)
+      const prepTime = findPrepTime($)
+      const cookTime = findCookTime($)
+      const totalTime = findTotalTime($)
 
-      console.log('Title: ', name)
-      console.log('imgUrl: ', imgUrl)
+      // console.log('Title: ', name)
+      // console.log('imgUrl: ', imgUrl)
       console.log('prep time: ', prepTime)
-      // console.log('findCookTime: ', cookTime)
+      console.log('cookTime: ', cookTime)
+      console.log('totalTime: ', totalTime)
       // console.log('Ingredients: ', ingredients)
       // console.log('Instructions: ', instructions)
 
