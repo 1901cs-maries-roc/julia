f11dea9190d24595f6bba787d0b2b1a424dbd038
Stephanie Coy <scoy028@gmail.com>
2019-03-21 15:46:20 -0400

    feat(form): add recipe form route

---
+++ client/components/recipe-form.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+class RecipeForm extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hey from recipe form</h1>
+      </div>
+    )
+  }
+}
+
+export default RecipeForm
