13a289aa0402026d56125c8aa685202ebd4e4aff
Stephanie Coy <scoy028@gmail.com>
2019-03-21 22:22:04 -0400

    feat(form): working on photo upload

--- client/components/recipe-form.js
+++ client/components/recipe-form.js
@@ -1,19 +1,58 @@
 import React, {Component} from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
-import Col from 'react-bootstrap/Col'
+// import Col from 'react-bootstrap/Col'
+import Figure from 'react-bootstrap/Figure'
+// import InputGroup from 'react-bootstrap/InputGroup'
+import PhotoAdd from './photo-add'
 
 class RecipeForm extends Component {
+  constructor() {
+    super()
+    this.createPhoto = this.createPhoto.bind(this)
+  }
+
+  createPhoto(newPhoto) {
+    console.log('PHOTO:', newPhoto.image)
+    const h = {} //headers
+    let data = new FormData()
+    data.append('image', newPhoto.image)
+    data.append('name', newPhoto.name)
+    h.Accept = 'application/json' //if you expect JSON response
+    fetch('/api/item', {
+      method: 'POST',
+      headers: h,
+      body: data
+    })
+      .then(response => {})
+      .catch(err => {
+        console.error(err)
+      })
+  }
+
   render() {
     return (
       <Form>
+        <Figure>
+          <Figure.Image
+            width={171}
+            height={180}
+            alt="171x180"
+            src="/imgPlaceholder.svg"
+          />
+          <PhotoAdd createPhoto={this.createPhoto} />
+          <Figure.Caption>Upload a picture here.</Figure.Caption>
+          <Button variant="primary" type="submit">
+            Upload
+          </Button>
+        </Figure>
         <Form.Row>
-          <Form.Group as={Col} md="2" controlId="formPrepTime">
+          <Form.Group controlId="formPrepTime">
             <Form.Label>Prep Time</Form.Label>
             <Form.Control type="prepTime" />
           </Form.Group>
 
-          <Form.Group as={Col} md="2" controlId="formCookTime">
+          <Form.Group controlId="formCookTime">
             <Form.Label>Cook Time</Form.Label>
             <Form.Control type="cookTime" />
           </Form.Group>
