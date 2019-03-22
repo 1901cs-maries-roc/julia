9c4cfc3808a8414c78c25ca5ffdd40fba542a25a
CelineChole <github@celinechole.com>
2019-03-22 12:15:08 -0400

    overview page css (#102)
    * fix: remove unsued button
    
    * style: recipe overview layout
    
    * fix: remove tags

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -11,7 +11,13 @@ import NavDropdown from 'react-bootstrap/NavDropdown'
 const NavigationBar = ({handleClick, isLoggedIn}) => (
   // error here: The prop `handleClick` is marked as required in `Bootstrap(undefined)`, but its value is `undefined`.
   // error here: The prop `isLoggedIn` is marked as required in `Bootstrap(undefined)`, but its value is `undefined`.
-  <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Navbar
+    collapseOnSelect
+    expand="lg"
+    bg="dark"
+    variant="dark"
+    className="navbar"
+  >
     <Navbar.Brand href="/">JULIA</Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
