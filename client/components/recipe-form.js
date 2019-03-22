0b8c44ce408ff8e8e256de55055df61d74e10c13
Stephanie Coy <scoy028@gmail.com>
2019-03-22 12:43:07 -0400

    feat(form): picture preview and pic upload display working

--- client/components/recipe-form.js
+++ client/components/recipe-form.js
@@ -14,18 +14,18 @@ class RecipeForm extends Component {
       manualEnter: false
     }
     this.createPhoto = this.createPhoto.bind(this)
-    this.manuallyEnter = this.manuallyEnter.bind(this)
+    // this.manuallyEnter = this.manuallyEnter.bind(this)
   }
 
-  manuallyEnter = event => {
-    console.log('in manually enter')
-    event.preventDefault()
-    console.log('state before', this.state)
-    this.setState = {
-      manualEnter: true
-    }
-    console.log('state after', this.state)
-  }
+  // manuallyEnter = event => {
+  //   console.log('in manually enter')
+  //   event.preventDefault()
+  //   console.log('state before', this.state)
+  //   this.setState = {
+  //     manualEnter: true
+  //   }
+  //   console.log('state after', this.state)
+  // }
 
   createPhoto(newPhoto) {
     console.log('PHOTO:', newPhoto.image)
@@ -46,12 +46,16 @@ class RecipeForm extends Component {
   }
 
   render() {
+    // console.log('state in render', this.state)
     return (
       <Container>
         <div>
           <h1>Add a recipe</h1>
         </div>
         <hr />
+        <div>
+          <h2>Add a recipe from a URL</h2>
+        </div>
         <InputGroup>
           <InputGroup.Prepend>
             <InputGroup.Text id="uploadUrl">Recipe URL</InputGroup.Text>
@@ -65,108 +69,98 @@ class RecipeForm extends Component {
             Upload
           </Button>
         </InputGroup>
-        <br />
-        {!this.state.manualEnter ? (
-          <div>
-            <Button
-              variant="primary"
-              type="button"
-              onClick={this.manuallyEnter}
-            >
-              Manually Enter a Recipe
-            </Button>
-            <hr />
-          </div>
-        ) : (
-          <Form>
-            <Figure>
-              <Figure.Image
-                width={171}
-                height={180}
-                alt="171x180"
-                src="/imgPlaceholder.svg"
+        <hr />
+        <div>
+          <h2>Manually Enter a Recipe</h2>
+        </div>
+        <Form>
+          {/* <Figure> */}
+          {/* <Figure.Image
+              width={171}
+              height={180}
+              alt="171x180"
+              src="/imgPlaceholder.svg"
+            /> */}
+          {/* <Figure.Caption>Upload a picture here.</Figure.Caption> */}
+          <PhotoAdd createPhoto={this.createPhoto} />
+          {/* <Button variant="primary" type="submit">
+              Upload
+            </Button> */}
+          {/* </Figure> */}
+          <Form.Row>
+            <Form.Group controlId="formPrepTime">
+              <Form.Label>Prep Time</Form.Label>
+              <Form.Control type="prepTime" />
+            </Form.Group>
+
+            <Form.Group controlId="formCookTime">
+              <Form.Label>Cook Time</Form.Label>
+              <Form.Control type="cookTime" />
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Row>
+            <Form.Group controlId="formWaitTime">
+              <Form.Label>Wait Time</Form.Label>
+              <Form.Control type="waitTime" />
+            </Form.Group>
+
+            <Form.Group controlId="servings">
+              <Form.Label>Number of servings</Form.Label>
+              <Form.Control type="servings" />
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Row>
+            <Form.Group md="6" controlId="formTitle">
+              <Form.Label>Recipe Title</Form.Label>
+              <Form.Control type="title" placeholder="Enter title" />
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Row>
+            <Form.Group controlId="formIngredients">
+              <Form.Label>Ingredients</Form.Label>
+              <Form.Control
+                type="ingredients"
+                placeholder="Enter one ingredient"
               />
-              <PhotoAdd createPhoto={this.createPhoto} />
-              <Figure.Caption>Upload a picture here.</Figure.Caption>
-              <Button variant="primary" type="submit">
-                Upload
-              </Button>
-            </Figure>
-            <Form.Row>
-              <Form.Group controlId="formPrepTime">
-                <Form.Label>Prep Time</Form.Label>
-                <Form.Control type="prepTime" />
-              </Form.Group>
+              <Form.Text className="text-muted">
+                Enter each ingredient on its own line.
+              </Form.Text>
+            </Form.Group>
+          </Form.Row>
 
-              <Form.Group controlId="formCookTime">
-                <Form.Label>Cook Time</Form.Label>
-                <Form.Control type="cookTime" />
-              </Form.Group>
-            </Form.Row>
+          <Form.Row>
+            <Form.Group controlId="formInstructions">
+              <Form.Label>Instructions</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows="2"
+                placeholder="Enter one instruction"
+              />
+              <Form.Text className="text-muted">
+                Enter each instruction on its own line.
+              </Form.Text>
+            </Form.Group>
+          </Form.Row>
 
-            <Form.Row>
-              <Form.Group controlId="formWaitTime">
-                <Form.Label>Wait Time</Form.Label>
-                <Form.Control type="waitTime" />
-              </Form.Group>
+          <Form.Row>
+            <Form.Group controlId="formTags">
+              <Form.Label>Tags</Form.Label>
+              <Form.Control as="select">
+                <option>Choose...</option>
+                <option>Easy</option>
+                <option>Intermediate</option>
+                <option>Hard</option>
+              </Form.Control>
+            </Form.Group>
+          </Form.Row>
 
-              <Form.Group controlId="servings">
-                <Form.Label>Number of servings</Form.Label>
-                <Form.Control type="servings" />
-              </Form.Group>
-            </Form.Row>
-
-            <Form.Row>
-              <Form.Group md="6" controlId="formTitle">
-                <Form.Label>Recipe Title</Form.Label>
-                <Form.Control type="title" placeholder="Enter title" />
-              </Form.Group>
-            </Form.Row>
-
-            <Form.Row>
-              <Form.Group controlId="formIngredients">
-                <Form.Label>Ingredients</Form.Label>
-                <Form.Control
-                  type="ingredients"
-                  placeholder="Enter one ingredient"
-                />
-                <Form.Text className="text-muted">
-                  Enter each ingredient on its own line.
-                </Form.Text>
-              </Form.Group>
-            </Form.Row>
-
-            <Form.Row>
-              <Form.Group controlId="formInstructions">
-                <Form.Label>Instructions</Form.Label>
-                <Form.Control
-                  as="textarea"
-                  rows="2"
-                  placeholder="Enter one instruction"
-                />
-                <Form.Text className="text-muted">
-                  Enter each instruction on its own line.
-                </Form.Text>
-              </Form.Group>
-            </Form.Row>
-
-            <Form.Row>
-              <Form.Group controlId="formTags">
-                <Form.Label>Tags</Form.Label>
-                <Form.Control as="select">
-                  <option>Choose...</option>
-                  <option>Easy</option>
-                  <option>Intermediate</option>
-                  <option>Hard</option>
-                </Form.Control>
-              </Form.Group>
-            </Form.Row>
-
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        )}
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
       </Container>
     )
   }
