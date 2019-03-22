9a7b00b3c4855fe67890b2623cb1d2877dcc31a5
celinechole <github@celinechole.com>
2019-03-22 12:06:42 -0400

    style: recipe overview layout

--- client/components/recipe-overview.js
+++ client/components/recipe-overview.js
@@ -3,6 +3,11 @@ import {connect} from 'react-redux'
 import {getRecipeThunk} from '../store'
 import IngredientsList from './ingredientsList'
 import {startCooking} from '../annyangCommands'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
 
 class RecipeOverview extends Component {
   constructor(props) {
@@ -37,52 +42,62 @@ class RecipeOverview extends Component {
     } = this.props.currentRecipe
 
     return (
-      <div>
-        <img src={imgUrl} />
-        <h1>Recipe Title: {name}</h1>
-        <p>Prep Time: {prepTime} minutes</p>
-        <p>Cooking Time: {cookTime} minutes</p>
-        <p>Wait Time: {waitTime} minutes</p>
-        <p>Serving Size: {serving}</p>
-        {/* add average time per user */}
-        <p>Average Time for Users: No user data at this time</p>
-        <div id="ingredient-list">
-          <p>Ingredients:</p>
-          {ingredients ? (
-            <IngredientsList ingredients={ingredients} isOverview={true} />
-          ) : (
-            <p>No Ingredients</p>
-          )}
-        </div>
-        <div id="recipe-steps">
-          <p>Instructions:</p>
-          <ol>
-            {steps ? (
-              steps.map(step => {
-                return <li key={step}>{step}</li>
-              })
-            ) : (
-              <li>No Steps</li>
-            )}
-          </ol>
-        </div>
-        <div id="tags">
-          {/* add reference to tags */}
-          <p>Tags:</p>
-          <ul>
-            {tags === [] ? (
-              tags.map(tag => {
-                return <li key={tag}>{tag}</li>
-              })
-            ) : (
-              <li>No tags are available</li>
-            )}
-          </ul>
-        </div>
-        <button type="button" onClick={this.handleClick}>
-          Start Cooking
-        </button>
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <img src={imgUrl} />
+          </Col>
+          <Col>
+            <h1>Recipe Title: {name}</h1>
+            <p>Prep Time: {prepTime} minutes</p>
+            <p>Cooking Time: {cookTime} minutes</p>
+            <p>Wait Time: {waitTime} minutes</p>
+            <p>Serving Size: {serving}</p>
+            {/* add average time per user */}
+            <p>Average Time for Users: No user data at this time</p>
+            <div id="ingredient-list">
+              <p>Ingredients:</p>
+              {ingredients ? (
+                <IngredientsList ingredients={ingredients} isOverview={true} />
+              ) : (
+                <p>No Ingredients</p>
+              )}
+            </div>
+          </Col>
+        </Row>
+        <Row className="row-grid">
+          <div id="recipe-steps">
+            <p>Instructions:</p>
+            <ol>
+              {steps ? (
+                steps.map(step => {
+                  return <li key={step}>{step}</li>
+                })
+              ) : (
+                <li>No Steps</li>
+              )}
+            </ol>
+          </div>
+          <div id="tags">
+            {/* add reference to tags */}
+            <p>Tags:</p>
+            <ul>
+              {tags === [] ? (
+                tags.map(tag => {
+                  return <li key={tag}>{tag}</li>
+                })
+              ) : (
+                <li>No tags are available</li>
+              )}
+            </ul>
+          </div>
+        </Row>
+        <Row className="row-grid">
+          <button type="button" onClick={this.handleClick}>
+            Start Cooking
+          </button>
+        </Row>
+      </Container>
     )
   }
 }
