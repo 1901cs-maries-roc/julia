592664d43820ebe3c2d2a055a125aa1e1e4111df
Wendy Sung <wendalssung@gmail.com>
2019-03-23 18:47:20 -0400

    refactoring on recipe nav

---
+++ client/components/recipe-step-nav.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import Col from 'react-bootstrap/Col'
+import Button from 'react-bootstrap/Button'
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
+
+export default function StepNav({stepIndex, steps, annyang, goBack, goToNext}) {
+  return (
+    <Col md={{span: 8, offset: 2}}>
+      <ButtonToolbar className="all-navigation-button">
+        <Button
+          className="navigation-button"
+          variant="secondary"
+          id="back"
+          type="button"
+          disabled={stepIndex === 0}
+          onClick={goBack}
+        >
+          Back
+        </Button>
+        <Button
+          className="navigation-button"
+          variant="success"
+          id="start"
+          type="button"
+          onClick={annyang}
+        >
+          Start
+        </Button>
+        <Button
+          className="navigation-button"
+          variant="danger"
+          id="pause"
+          type="button"
+          onClick={() => stop()}
+        >
+          Stop
+        </Button>
+        <Button
+          className="navigation-button"
+          variant="secondary"
+          id="next"
+          disabled={stepIndex >= steps.length - 1}
+          type="button"
+          onClick={goToNext}
+        >
+          Next
+        </Button>
+        <Button variant="secondary" type="submit" className="navigation-button">
+          Help
+        </Button>
+      </ButtonToolbar>
+    </Col>
+  )
+}
