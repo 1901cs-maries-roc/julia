de42bdadf00eca9e92f3475bd7017fa2117c05d9
celinechole <github@celinechole.com>
2019-03-23 07:40:14 -0400

    modal positioning

--- client/components/recipe-step.js
+++ client/components/recipe-step.js
@@ -136,7 +136,7 @@ class RecipeStep extends Component {
           </Col>
           {this.state.isListening && (
             <Portal>
-              <Modal.Dialog size="sm" id="modal">
+              <Modal.Dialog id="modal">
                 <Modal.Header closeButton onHide={this.handleStop}>
                   <Modal.Title>Julia Listening</Modal.Title>
                 </Modal.Header>
