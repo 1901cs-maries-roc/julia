9f77ebe350b3ee867d9c0b6d46097e29186fdbfa
celinechole <github@celinechole.com>
2019-03-24 09:17:59 -0400

    title oustside the form component

--- client/components/recipe-form.js
+++ client/components/recipe-form.js
@@ -85,105 +85,105 @@ class RecipeForm extends Component {
         </Row>
 
         <Row className="row-grid">
-          <Form>
-            <h4>Manually Enter a Recipe</h4>
-
-            <Row>
-              <Col>
-                <PhotoAdd createPhoto={this.createPhoto} />
-                <Form.Row>
-                  <Form.Group controlId="formPrepTime">
-                    <Form.Label>Prep Time</Form.Label>
-                    <Form.Control
-                      type="number"
-                      id="prepTime"
-                      onChange={this.handleChange}
-                    />
-                  </Form.Group>
-
-                  <Form.Group controlId="formCookTime">
-                    <Form.Label>Cook Time</Form.Label>
-                    <Form.Control
-                      type="number"
-                      id="cookTime"
-                      onChange={this.handleChange}
-                    />
-                  </Form.Group>
-                </Form.Row>
-
-                <Form.Row>
-                  <Form.Group controlId="formWaitTime">
-                    <Form.Label>Wait Time</Form.Label>
-                    <Form.Control
-                      type="number"
-                      id="waitTime"
-                      onChange={this.handleChange}
-                    />
-                  </Form.Group>
-
-                  <Form.Group controlId="serving">
-                    <Form.Label>Number of servings</Form.Label>
-                    <Form.Control
-                      type="number"
-                      id="serving"
-                      onChange={this.handleChange}
-                    />
-                  </Form.Group>
-                </Form.Row>
-              </Col>
-              <Col>
-                <Form.Row>
-                  <Form.Group md="6" controlId="formName">
-                    <Form.Label>Recipe Title</Form.Label>
-                    <Form.Control
-                      id="name"
-                      placeholder="Enter title"
-                      onChange={this.handleChange}
-                    />
-                  </Form.Group>
-                </Form.Row>
-
-                <Form.Row>
-                  <Form.Group controlId="formIngredients">
-                    <Form.Label>Ingredients</Form.Label>
-                    <Form.Control
-                      id="ingredients"
-                      placeholder="Enter one ingredient"
-                      onChange={this.handleChange}
-                    />
-                    <Form.Text className="text-muted">
-                      Enter each ingredient on its own line.
-                    </Form.Text>
-                  </Form.Group>
-                </Form.Row>
-
-                <Form.Row>
-                  <Form.Group controlId="formInstructions">
-                    <Form.Label>Instructions</Form.Label>
-                    <Form.Control
-                      as="textarea"
-                      rows="2"
-                      id="steps"
-                      placeholder="Enter one instruction"
-                      onChange={this.handleChange}
-                    />
-                    <Form.Text className="text-muted">
-                      Enter each instruction on its own line.
-                    </Form.Text>
-                  </Form.Group>
-                </Form.Row>
-
-                <Button
-                  variant="primary"
-                  type="submit"
-                  onClick={this.handleSubmit}
-                >
-                  Submit
-                </Button>
-              </Col>
-            </Row>
-          </Form>
+          <h4>Manually Enter a Recipe</h4>
         </Row>
+        <Form>
+          <Row>
+            <Col>
+              <PhotoAdd createPhoto={this.createPhoto} />
+              <Form.Row>
+                <Form.Group controlId="formPrepTime">
+                  <Form.Label>Prep Time</Form.Label>
+                  <Form.Control
+                    type="number"
+                    id="prepTime"
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="formCookTime">
+                  <Form.Label>Cook Time</Form.Label>
+                  <Form.Control
+                    type="number"
+                    id="cookTime"
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
+              </Form.Row>
+            </Col>
+            <Col>
+              <Form.Row>
+                <Form.Group controlId="formWaitTime">
+                  <Form.Label>Wait Time</Form.Label>
+                  <Form.Control
+                    type="number"
+                    id="waitTime"
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
+
+                <Form.Group controlId="serving">
+                  <Form.Label>Number of servings</Form.Label>
+                  <Form.Control
+                    type="number"
+                    id="serving"
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
+              </Form.Row>
+            </Col>
+            <Col>
+              <Form.Row>
+                <Form.Group controlId="formName">
+                  <Form.Label>Recipe Title</Form.Label>
+                  <Form.Control
+                    id="name"
+                    placeholder="Enter title"
+                    onChange={this.handleChange}
+                  />
+                </Form.Group>
+              </Form.Row>
+
+              <Form.Row>
+                <Form.Group controlId="formIngredients">
+                  <Form.Label>Ingredients</Form.Label>
+                  <Form.Control
+                    id="ingredients"
+                    placeholder="Enter one ingredient"
+                    onChange={this.handleChange}
+                  />
+                  <Form.Text className="text-muted">
+                    Enter each ingredient on its own line.
+                  </Form.Text>
+                </Form.Group>
+              </Form.Row>
+
+              <Form.Row>
+                <Form.Group controlId="formInstructions">
+                  <Form.Label>Instructions</Form.Label>
+                  <Form.Control
+                    as="textarea"
+                    rows="2"
+                    id="steps"
+                    placeholder="Enter one instruction"
+                    onChange={this.handleChange}
+                  />
+                  <Form.Text className="text-muted">
+                    Enter each instruction on its own line.
+                  </Form.Text>
+                </Form.Group>
+              </Form.Row>
+
+              <Button
+                variant="primary"
+                type="submit"
+                onClick={this.handleSubmit}
+              >
+                Submit
+              </Button>
+            </Col>
+          </Row>
+        </Form>
       </Container>
     )
   }
