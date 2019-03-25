f84bd73c49734dbfd1568357398dc344030a1b7f
Stephanie Coy <scoy028@gmail.com>
2019-03-24 20:57:30 -0400

    feat(oauth): added google oauth credentials

--- server/index.js
+++ server/index.js
@@ -54,7 +54,7 @@ const createApp = () => {
   // session middleware with passport
   app.use(
     session({
-      secret: process.env.SESSION_SECRET || 'my best friend is Cody',
+      secret: process.env.SESSION_SECRET || 'julia child rocks!',
       store: sessionStore,
       resave: false,
       saveUninitialized: false
