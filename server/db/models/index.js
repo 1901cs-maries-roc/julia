6866914ea23fd66ebc1ad1817391523d0953d749
marilyncs <marilyn.n.chu@gmail.com>
2019-03-25 00:48:56 -0400

    feat(userprofile): added a through table for favRecipes

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,15 +4,20 @@ const Tag = require('./tag')
 const db = require('../db')
 
 const tagRecipe = db.define('tagRecipe')
+const favRecipes = db.define('favRecipes')
 
 Recipe.belongsTo(User)
 User.hasMany(Recipe)
 Tag.belongsToMany(Recipe, {through: tagRecipe})
 Recipe.belongsToMany(Tag, {through: tagRecipe})
 
+User.belongsToMany(Recipe, {through: favRecipes})
+Recipe.belongsToMany(User, {through: favRecipes})
+
 module.exports = {
   User,
   Tag,
   Recipe,
-  tagRecipe
+  tagRecipe,
+  favRecipes
 }
