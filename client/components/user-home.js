bca5ae5db37bbc8bb506e53b6ec8e19efce0b72d
marilyncs <marilyn.n.chu@gmail.com>
2019-03-25 01:20:27 -0400

    feat(userprofile): added template for user profile

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import Card from 'react-bootstrap/Card'
+import Media from 'react-bootstrap/Media'
+import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 /**
  * COMPONENT
@@ -9,9 +15,90 @@ export const UserHome = props => {
   const {email} = props
 
   return (
-    <div>
-      <h3>Welcome, {email}</h3>
-    </div>
+    <Container className="container">
+      <Row>
+        <Col md={{span: 4}}>
+          <Card style={{width: '18rem'}}>
+            <Card.Img variant="top" src="/placeholder.svg" />
+            <Card.Body>
+              <Card.Title>Welcome, {email}</Card.Title>
+              <Card.Text>Quick blurb about user</Card.Text>
+            </Card.Body>
+            <Card.Body>
+              <Card.Link href="/favorites">My Favorites</Card.Link>
+              <Card.Link href="#">Edit Profile</Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col md={{span: 6}}>
+          <h3>My Recipes:</h3>
+          <Media>
+            <img
+              width={64}
+              height={64}
+              className="align-self-start mr-3"
+              src="/placeholderSM.svg"
+              alt="small placeholder"
+            />
+            <Media.Body>
+              <h5>Media Heading</h5>
+              <p>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+                scelerisque ante sollicitudin commodo. Cras purus odio,
+                vestibulum in vulputate at,
+              </p>
+              <Button className="navigation-button" variant="secondary">
+                Delete
+              </Button>
+            </Media.Body>
+          </Media>
+          <br />
+          <hr />
+          <Media>
+            <img
+              width={64}
+              height={64}
+              className="align-self-start mr-3"
+              src="/placeholderSM.svg"
+              alt="small placeholder"
+            />
+            <Media.Body>
+              <h5>Media Heading</h5>
+              <p>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+                scelerisque ante sollicitudin commodo. Cras purus odio,
+                vestibulum in vulputate at,
+              </p>
+              <Button className="navigation-button" variant="secondary">
+                Delete
+              </Button>
+            </Media.Body>
+          </Media>
+          <br />
+          <hr />
+          <Media>
+            <img
+              width={64}
+              height={64}
+              className="align-self-start mr-3"
+              src="/placeholderSM.svg"
+              alt="small placeholder"
+            />
+            <Media.Body>
+              <h5>Media Heading</h5>
+              <p>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus
+                scelerisque ante sollicitudin commodo. Cras purus odio,
+                vestibulum in vulputate at,
+              </p>
+              <Button className="navigation-button" variant="secondary">
+                Delete
+              </Button>
+            </Media.Body>
+          </Media>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
