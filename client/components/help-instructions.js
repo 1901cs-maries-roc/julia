b9f29002e9b7d1d3c92763a6683852a12f287ba5
marilyncs <45369255+marilyncs@users.noreply.github.com>
2019-03-25 09:55:17 -0400

    fixed help button  (#145)
    * feat(recipeStep/help): started to import popover overlays
    
    * feat(help): version 1, but state not showing when first on page
    
    * feat(help): added some nav things
    
    * feat(help/nav): fixed help button to display instructions and added logo/nav css overrides
    
    * fix(navbar): commented out navbar css override color
    
    * fix(help): fixed merge conflicts on help button/nav

---
+++ client/components/help-instructions.js
@@ -0,0 +1,70 @@
+import React, {Component} from 'react'
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar'
+import Button from 'react-bootstrap/Button'
+import Overlay from 'react-bootstrap/Overlay'
+import Popover from 'react-bootstrap/Popover'
+
+class HelpInstructions extends Component {
+  constructor(props, context) {
+    super(props, context)
+
+    this.handleClick = ({target}) => {
+      this.setState(s => ({target, show: !s.show}))
+    }
+    this.attachRef = target => this.setState({target})
+    this.state = {
+      show: true
+    }
+  }
+
+  render() {
+    const {show, target} = this.state
+    return (
+      <div>
+        <ButtonToolbar>
+          <Button
+            className="navigation-button"
+            variant="secondary"
+            ref={this.attachRef}
+            id="help"
+            onClick={this.handleClick}
+          >
+            Help
+          </Button>
+          <Overlay
+            target={target}
+            show={show}
+            placement="right"
+            container={this}
+            containerPadding={20}
+          >
+            <Popover id="popover-contained" title="How to Use Julia">
+              <br />
+              <i className="far fa-play-circle" fa-10x />
+              <strong> Press START to enable Julia</strong> <br />
+              {/* <img src={process.env.PUBLIC_URL + '/start-button.png'} alt="start-button" /> */}
+              <hr />
+              You can ask Julia any of the following commands preceeded with
+              "Hey Julia"
+              <hr />
+              <strong>Commands: </strong> <br />
+              "Hey Julia Help" <br />
+              "Hey Julia Repeat" <br />
+              "Hey Julia Back" <br />
+              "Hey Julia Next" <br />
+              "Hey Julia Ingredients" <br />
+              "Hey Julia Instructions" <br />
+              "Hey Julia Stop" <br />
+              "Hey Julia Back to Recipe" <br />
+              <br />
+              <Button size="sm" onClick={() => this.setState({show: !show})}>
+                GOT IT
+              </Button>
+            </Popover>
+          </Overlay>
+        </ButtonToolbar>
+      </div>
+    )
+  }
+}
+export default HelpInstructions
