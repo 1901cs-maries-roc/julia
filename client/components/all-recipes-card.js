7a695af998853fe051c27e488cfe711c47ba8c9e
celinechole <github@celinechole.com>
2019-03-26 12:21:21 -0400

    style: less padding in card body, decrease height

--- client/components/all-recipes-card.js
+++ client/components/all-recipes-card.js
@@ -6,8 +6,8 @@ const OneRecipeCard = props => {
   return (
     <Card className="recipe-card">
       <Card.Img variant="top" src={recipe.imgUrl} className="img-resize" />
-      <Card.Body className="test">
-        <Card.Text>{recipe.name}</Card.Text>
+      <Card.Body className="card-body">
+        <Card.Text className="card-text">{recipe.name}</Card.Text>
       </Card.Body>
     </Card>
   )
