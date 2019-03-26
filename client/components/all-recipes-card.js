2085b00f76b8c8e619968753c3c3e74eb2005bad
celinechole <github@celinechole.com>
2019-03-26 12:51:29 -0400

    feat: slice recipe title if too long to fit on one line

--- client/components/all-recipes-card.js
+++ client/components/all-recipes-card.js
@@ -6,15 +6,7 @@ const OneRecipeCard = props => {
   let name = recipe.name
   if (name.length > 40) {
     name = recipe.name.substring(0, 40)
-    console.log('1 ', recipe.name)
-    // recipe.name = recipe.name.substring(0, /\s(?=\S*$)/)
-    // let reg = /\s(?=\S*$)/
-    console.log(name.lastIndexOf(' '))
-    console.log(name.substring(0, name.lastIndexOf(' ')))
     name = name.substring(0, name.lastIndexOf(' '))
-
-    console.log('char at ', recipe.name.indexOf)
-    console.log(recipe.name.substring(0, /\s(?=\S*$)/))
   }
   return (
     <Card className="recipe-card">
