43eea9ad9bb01adb317bacd232be4db5636b272a
sungsauce <35469813+sungsauce@users.noreply.github.com>
2019-03-27 17:06:19 -0400

    add transcript placeholder for Julia UI (#190)

--- client/components/recipe-step-julia.js
+++ client/components/recipe-step-julia.js
@@ -3,19 +3,33 @@ import Spinner from 'react-bootstrap/Spinner'
 
 export default function Julia({listening, processing}) {
   // have placeholder Julia with prompt
+  let voiceState
+  if (listening && processing) {
+    voiceState = {
+      state: 'processing',
+      micState: 'mic-processing',
+      displayTxt: 'Thinking...'
+    }
+  } else if (listening && !processing) {
+    voiceState = {
+      state: 'standby',
+      micState: 'mic-listening',
+      displayTxt: 'Listening...'
+    }
+  } else {
+    voiceState = {
+      state: 'off',
+      micState: 'mic-off',
+      displayTxt: "Hi, I'm Julia. Press play to begin."
+    }
+  }
   return (
-    listening &&
-    (processing ? (
-      <div className="julia">
+    <div className="julia">
+      {voiceState.state === 'processing' && (
         <Spinner animation="grow" variant="dark" />
-        <i className="fas fa-microphone fa-2x microphone-off" />
-        <span className="micro-text">Thinking...</span>
-      </div>
-    ) : (
-      <div className="julia">
-        <i className="fas fa-microphone fa-2x microphone-on" />
-        <span className="micro-text">Listening...</span>
-      </div>
-    ))
+      )}
+      <i className={`fas fa-microphone fa-2x ${voiceState.micState}`} />
+      <span className="micro-text">{voiceState.displayTxt}</span>
+    </div>
   )
 }
